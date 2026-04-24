2f6418ce4acebe9b3c29f4f4152891d3754d1d0d
whmdg20090421 <whmdg20090421@users.noreply.github.com>
2026-04-24 04:15:48 +0000

    fix(build): 修复引入 path_provider 时的语法错误并升级版本到 1.1.0

--- english_dictation_flutter/lib/main.dart
+++ english_dictation_flutter/lib/main.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:path_provider/package:path_provider.dart';
+import 'package:path_provider/path_provider.dart';
 import 'screens/splash_screen.dart';
 import 'theme.dart';
 import 'providers/dictation_provider.dart';
